56a72b14688ec8380e2a9233c131c82d87dcdc84
iTs-Spooky <shakyaanish69@gmail.com>
2020-04-29 18:37:32 +0545

    tic tac toe

---
+++ tictactoe.py
@@ -0,0 +1,17 @@
+#Board
+#Display board
+#play game
+#Player flip
+#handle turn
+#check win 
+  #check rows
+  #check columns
+  #check diagonals
+#check tie
+
+
+Board=["-", "-", "-"
+       "-", "-", "-"
+       "-", "-", "-"]
+
+
